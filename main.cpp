7afae1dc747e0fc9a258136b38c56418e9f25472
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2025-10-04 16:23:08 -0700

    main.cpp now outputs to 3 different output files

--- main.cpp
+++ main.cpp
@@ -8,43 +8,48 @@
 
 int main() {
     // Prompt user for an input file to parse
-    std::string filein;
-    std::cout << "Enter input file: ";
-    std::cin >> filein;
+    std::vector<std::string> filesin = {"test_small.txt", "test_medium.txt", "test_large.txt"};
 
-    // Check if file exists
-    std::ifstream file(filein);
-    if (!file) {
-        std::cerr << "Error: could not open input file\n";
-        return 1;
+    // Loop files, check if file exists
+    for(std::string filein : filesin) {
+        std::ifstream file(filein);
+        if (!file) {
+            std::cerr << "Error: could not open input file: " << filein << "\n";
+            return 1;
+        }
+    
+
+        // Put all of source code into one long string
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        std::string sourceCode = buffer.str();
+        file.close();
+
+        // Initialize our lexer, then get all tokens from sourceCode
+        Lexer lexer(sourceCode);
+        std::vector<Token> tokens = lexer.getAllTokens();
+
+        // Opens an output file for writing results
+        std::string outputFileName = filein.substr(0, filein.length() - 4) + "_output.txt";
+        std::ofstream outFile(outputFileName);
+
+        std::cout << std::left << "----------------" << filein << "----------------" << "\n";
+
+        std::cout << std::left << std::setw(15) << "token" << std::setw(6) << "|" << "lexeme" << "\n";
+        outFile << std::left << std::setw(15) << "token" << std::setw(6) << "|" << "lexeme\n";
+
+        std::cout << std::endl;
+        outFile << std::endl;
+
+        // Print tokens
+        for (auto& token : tokens) {
+            std::string category = lexer.getCategoryName(token.type);
+
+            std::cout << std::left << std::setw(15) << category << std::setw(6) << "|"<< token.value << "\n";
+            outFile << std::left << std::setw(15) << category << std::setw(6) << "|" << token.value << "\n";
+        }
+        
+        outFile.close();
     }
-
-    // Put all of source code into one long string
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    std::string sourceCode = buffer.str();
-    file.close();
-
-    // Initialize our lexer, then get all tokens from sourceCode
-    Lexer lexer(sourceCode);
-    std::vector<Token> tokens = lexer.getAllTokens();
-
-    // Opens an output file for writing results
-    std::ofstream outFile("tokens_output.txt");
-
-    std::cout << std::left << std::setw(15) << "token" << std::setw(6) << "|" << "lexeme" << "\n";
-    outFile << std::left << std::setw(15) << "token" << std::setw(6) << "|" << "lexeme\n";
-
-    std::cout << std::endl;
-    outFile << std::endl;
-
-    // Print tokens
-    for (auto& token : tokens) {
-        std::string category = lexer.getCategoryName(token.type);
-
-        std::cout << std::left << std::setw(15) << category << std::setw(6) << "|"<< token.value << "\n";
-        outFile << std::left << std::setw(15) << category << std::setw(6) << "|" << token.value << "\n";
-    }
-    outFile.close();
     return 0;
 }
